4ad30ab1cdce4e49e1aa81d51ee3d861930412d4
jhauptma <84292598+jhauptma12357@users.noreply.github.com>
2024-11-26 04:07:29 -0500

    Cleaned up code a bit more and fix res
    Made AnimationLoader and ImageLoader work with a bundled jar.

--- src/Media/AnimationLoader.java
+++ src/Media/AnimationLoader.java
@@ -11,28 +11,29 @@ public class AnimationLoader {
 	private static final Map<String, Animation> animationMap = new HashMap<>();
 
 	public static void loadAnimations() throws SlickException {
-		animationMap.put("playerMovingRight", new Animation(
-				new SpriteSheet("res/animations/Player/playerRunningRight.png", 140, 186), 50));
-		animationMap.put("credits", new Animation(
-				new SpriteSheet("res/animations/levelSelect/portalCredits.png", 320, 320), 40));
-		animationMap.put("cooldown", new Animation(
-				new SpriteSheet("res/animations/Player/cooldown.png", 128, 128), 116));
-		animationMap.put("playerMovingLeft", new Animation(
-				new SpriteSheet("res/animations/Player/playerRunningLeft.png", 140, 186), 50));
-		animationMap.put("playerMovingRightUpsideDown", new Animation(
-				new SpriteSheet("res/animations/Player/playerUpsideDownRunningRight.png", 140, 186),
-				50));
-		animationMap.put("playerMovingLeftUpsideDown", new Animation(
-				new SpriteSheet("res/animations/Player/playerUpsideDownRunningLeft.png", 140, 186),
-				50));
-		animationMap.put("portal", new Animation(
-				new SpriteSheet("res/animations/levelSelect/portal.png", 320, 320), 40));
-		animationMap.put("openedPortal", new Animation(
-				new SpriteSheet("res/animations/levelSelect/openPortal.png", 500, 500), 15));
-		animationMap.put("key",
-				new Animation(new SpriteSheet("res/animations/Puzzle/key.png", 180, 180), 25));
-		animationMap.put("door",
-				new Animation(new SpriteSheet("res/animations/Puzzle/door.png", 600, 600), 100));
+		animationMap.put("playerMovingRight",
+				makeAnimation("animations/Player/playerRunningRight.png", 140, 186, 50));
+		animationMap.put("credits",
+				makeAnimation("animations/levelSelect/portalCredits.png", 320, 320, 40));
+		animationMap.put("cooldown",
+				makeAnimation("animations/Player/cooldown.png", 128, 128, 116));
+		animationMap.put("playerMovingLeft",
+				makeAnimation("animations/Player/playerRunningLeft.png", 140, 186, 50));
+		animationMap.put("playerMovingRightUpsideDown", makeAnimation(
+				"animations/Player/playerUpsideDownRunningRight.png", 140, 186, 50));
+		animationMap.put("playerMovingLeftUpsideDown", makeAnimation(
+				"animations/Player/playerUpsideDownRunningLeft.png", 140, 186, 50));
+		animationMap.put("portal",
+				makeAnimation("animations/levelSelect/portal.png", 320, 320, 40));
+		animationMap.put("openedPortal",
+				makeAnimation("animations/levelSelect/openPortal.png", 500, 500, 15));
+		animationMap.put("key", makeAnimation("animations/Puzzle/key.png", 180, 180, 25));
+		animationMap.put("door", makeAnimation("animations/Puzzle/door.png", 600, 600, 100));
+	}
+
+	private static Animation makeAnimation(String path, int tw, int th, int d) throws SlickException {
+		SpriteSheet ss = new SpriteSheet(path,AnimationLoader.class.getClassLoader().getResourceAsStream(path), tw, th);
+		return new Animation(ss,d);
 	}
 
 	public static Animation returnAnimation(String key) throws SlickException {
