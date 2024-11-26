4ad7190a9e9ec7371d0584a5303e50d44a92e9f9
jhauptma <84292598+jhauptma12357@users.noreply.github.com>
2024-11-26 04:07:29 -0500

    Cleaned up code a bit more and fix res
    Made AnimationLoader and ImageLoader work with a bundled jar.

--- src/Media/ImageLoader.java
+++ src/Media/ImageLoader.java
@@ -21,52 +21,57 @@ public class ImageLoader {
 	private static final Map<String, Image> imageMap = new HashMap<>();
 
 	public static void loadImages() throws SlickException {
-		midgameLogo = new Image("res/images/GUI/Midgame1.png");
+
+		midgameLogo = makeImage("images/GUI/Midgame1.png");
 		midgameLogonoBGScaled = midgameLogo.getScaledCopy(Main.getScreenWidth(),
 				Main.getScreenHeight());
 
-		cursor = new Image("res/images/GUI/cursor.png");
+		cursor = makeImage("images/GUI/cursor.png");
 
-		imageMap.put("gravityUp", new Image("res/images/Arrows/leverUp.png"));
-		imageMap.put("cooldown", new Image("res/images/GUI/readyCooldown.png"));
-		imageMap.put("gravityDown", new Image("res/images/Arrows/leverDown.png"));
-		imageMap.put("gravityRight", new Image("res/images/Arrows/leverRight.png"));
-		imageMap.put("gravityLeft", new Image("res/images/Arrows/leverLeft.png"));
-		imageMap.put("AFKLeft", new Image("res/images/Player/AFKLeft.png"));
-		imageMap.put("AFKRight", new Image("res/images/Player/AFKRight.png"));
-		imageMap.put("AFKUpsideDownLeft", new Image("res/images/Player/AFKUpsideDownLeft.png"));
-		imageMap.put("AFKUpsideDownRight", new Image("res/images/Player/AFKUpsideDownRight.png"));
-		imageMap.put("glow", new Image("res/images/white.png"));
-		imageMap.put("door", new Image("res/images/Puzzle/door.png"));
-		imageMap.put("spike", new Image("res/images/Hazards/spike.png"));
-		imageMap.put("lvl1", new Image("res/images/levelNumbers/One.png"));
-		imageMap.put("lvl2", new Image("res/images/levelNumbers/Two.png"));
-		imageMap.put("lvl3", new Image("res/images/levelNumbers/Three.png"));
-		imageMap.put("lvl4", new Image("res/images/levelNumbers/Four.png"));
-		imageMap.put("lvl5", new Image("res/images/levelNumbers/Five.png"));
-		imageMap.put("lvl6", new Image("res/images/levelNumbers/Six.png"));
-		imageMap.put("lvl7", new Image("res/images/levelNumbers/Seven.png"));
-		imageMap.put("lvl8", new Image("res/images/levelNumbers/Eight.png"));
-		imageMap.put("pressE", new Image("res/images/levelNumbers/pressE.png"));
-		imageMap.put("E", new Image("res/images/GUI/E.png"));
-		imageMap.put("lvlCredits", new Image("res/images/levelNumbers/lvlCredits.png"));
-		imageMap.put("credits", new Image("res/images/Credits/Credits.png"));
-		imageMap.put("rate", new Image("res/images/Credits/rate.png"));
-		imageMap.put("thanks", new Image("res/images/Credits/thanks.png"));
-		imageMap.put("thumbsup", new Image("res/images/Credits/thumbsup.png"));
-		imageMap.put("openDoor", new Image("res/images/Puzzle/openDoor.png"));
-		imageMap.put("welcome", new Image("res/images/GUI/welcome.png"));
-		imageMap.put("youDied", new Image("res/images/GUI/youDied.png"));
-		imageMap.put("ghostBlock", new Image("res/images/Puzzle/ghostBlock.png"));
-		imageMap.put("downGhostBlock", new Image("res/images/Puzzle/downGhostBlock.png"));
-		imageMap.put("ghostBlockText", new Image("res/images/Text/ghostBlockText.png"));
+		imageMap.put("gravityUp", makeImage("images/Arrows/leverUp.png"));
+		imageMap.put("cooldown", makeImage("images/GUI/readyCooldown.png"));
+		imageMap.put("gravityDown", makeImage("images/Arrows/leverDown.png"));
+		imageMap.put("gravityRight", makeImage("images/Arrows/leverRight.png"));
+		imageMap.put("gravityLeft", makeImage("images/Arrows/leverLeft.png"));
+		imageMap.put("AFKLeft", makeImage("images/Player/AFKLeft.png"));
+		imageMap.put("AFKRight", makeImage("images/Player/AFKRight.png"));
+		imageMap.put("AFKUpsideDownLeft", makeImage("images/Player/AFKUpsideDownLeft.png"));
+		imageMap.put("AFKUpsideDownRight", makeImage("images/Player/AFKUpsideDownRight.png"));
+		imageMap.put("glow", makeImage("images/white.png"));
+		imageMap.put("door", makeImage("images/Puzzle/door.png"));
+		imageMap.put("spike", makeImage("images/Hazards/spike.png"));
+		imageMap.put("lvl1", makeImage("images/levelNumbers/One.png"));
+		imageMap.put("lvl2", makeImage("images/levelNumbers/Two.png"));
+		imageMap.put("lvl3", makeImage("images/levelNumbers/Three.png"));
+		imageMap.put("lvl4", makeImage("images/levelNumbers/Four.png"));
+		imageMap.put("lvl5", makeImage("images/levelNumbers/Five.png"));
+		imageMap.put("lvl6", makeImage("images/levelNumbers/Six.png"));
+		imageMap.put("lvl7", makeImage("images/levelNumbers/Seven.png"));
+		imageMap.put("lvl8", makeImage("images/levelNumbers/Eight.png"));
+		imageMap.put("pressE", makeImage("images/levelNumbers/pressE.png"));
+		imageMap.put("E", makeImage("images/GUI/E.png"));
+		imageMap.put("lvlCredits", makeImage("images/levelNumbers/lvlCredits.png"));
+		imageMap.put("credits", makeImage("images/Credits/Credits.png"));
+		imageMap.put("rate", makeImage("images/Credits/rate.png"));
+		imageMap.put("thanks", makeImage("images/Credits/thanks.png"));
+		imageMap.put("thumbsup", makeImage("images/Credits/thumbsup.png"));
+		imageMap.put("openDoor", makeImage("images/Puzzle/openDoor.png"));
+		imageMap.put("welcome", makeImage("images/GUI/welcome.png"));
+		imageMap.put("youDied", makeImage("images/GUI/youDied.png"));
+		imageMap.put("ghostBlock", makeImage("images/Puzzle/ghostBlock.png"));
+		imageMap.put("downGhostBlock", makeImage("images/Puzzle/downGhostBlock.png"));
+		imageMap.put("ghostBlockText", makeImage("images/Text/ghostBlockText.png"));
 		imageMap.put("spike2", imageMap.get("spike").getFlippedCopy(false, true));
-		imageMap.put("iceText2", new Image("res/images/Text/iceText2.png"));
-		imageMap.put("newIce", new Image("res/images/Text/newIce.png"));
+		imageMap.put("iceText2", makeImage("images/Text/iceText2.png"));
+		imageMap.put("newIce", makeImage("images/Text/newIce.png"));
 	}
 
 	public static Image getCursor() {
 		return cursor;
+	}
+
+	private static Image makeImage(String path) throws SlickException {
+		return new Image(ImageLoader.class.getClassLoader().getResourceAsStream(path), path, false);
 	}
 
 	public static Image returnImages(String s) throws SlickException {
